de20c536b61000c83558e9e703bc6c8cd008fedb
e.akhantev <e.akhantev@s7.ru>
2023-08-03 14:32:18 +0400

    feat: create function `reverse`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+   let numberArr = String(n).split('');
+
+   if (numberArr.slice(-1) == '0') {
+      numberArr.pop();
+   } 
+   
+   if (numberArr[0] == '-') {
+      numberArr.shift();
+   } 
+
+   return Number(numberArr.reverse().join(''));
 }
